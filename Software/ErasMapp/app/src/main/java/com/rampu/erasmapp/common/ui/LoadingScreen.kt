cd6af31b7bebdd88a0027d6675dda2cc09481f47
lkanjir23 <lkanjir23@student.foi.hr>
2026-02-03 19:39:32 +0100

    #43 - Designed the loading screen

--- Software/ErasMapp/app/src/main/java/com/rampu/erasmapp/common/ui/LoadingScreen.kt
+++ Software/ErasMapp/app/src/main/java/com/rampu/erasmapp/common/ui/LoadingScreen.kt
@@ -1,19 +1,45 @@
 package com.rampu.erasmapp.common.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rampu.erasmapp.common.ui.components.LoadingIndicator
+import com.rampu.erasmapp.common.ui.components.Logo
+import com.rampu.erasmapp.ui.theme.ErasMappTheme
 
 @Composable
-fun LoadingScreen(){
+fun LoadingScreen() {
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text("Loading screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Logo(
+            modifier = Modifier
+                .size(150.dp)
+        )
+        Spacer(Modifier.height(10.dp))
+        LoadingIndicator();
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LoadingScreenPreview() {
+    ErasMappTheme {
+        LoadingScreen()
     }
 }
